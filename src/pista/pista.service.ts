a08cc5a14d1486f2f7498456b8745d4b19931ce1
mauri52 <mauricio.islas@gmail.com>
2022-09-21 15:43:46 -0300

    paso 2.1 - servidor mockeado por api-rest - datos arreglo

--- src/pista/pista.service.ts
+++ src/pista/pista.service.ts
@@ -3,19 +3,17 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class PistaService {
     private listaPistas = [];
-    private static readonly CANTIDAD_PISTAS = 5;
 
     public getPistas() : any {
-        this.listaPistas = [];
-        for (let i = 0; i < PistaService.CANTIDAD_PISTAS; i++) {
-            let pista = {
-                "identificador" : i+1,
-                "titulo" : `titulo ${i+1}`,
-                "duracion" : Math.floor(Math.random() * 300),
-                "interprete" : `interprete ${Math.floor(Math.random() * 3)+1}`
-            }
-            this.listaPistas.push(pista);
-        }
+        this.listaPistas = [
+            { "identificador" : 1, "titulo" : "Bohemian Rhapsody", "duracion" : 481, "interprete" : "Queen" },
+            { "identificador" : 2, "titulo" : "Imagine", "duracion" : 290, "interprete" : "John Lennon" },
+            { "identificador" : 3, "titulo" : "Satisfaction", "duracion" : 308, "interprete" : "The Rolling Stones" },
+            { "identificador" : 4, "titulo" : "HighWay to Hell", "duracion" : 241, "interprete" : "AC/DC" },
+            { "identificador" : 5, "titulo" : "November Rain", "duracion" : 255, "interprete" : "Guns & Roses" },
+            { "identificador" : 6, "titulo" : "Jaded", "duracion" : 255, "interprete" : "Aerosmith" },
+            { "identificador" : 7, "titulo" : "I Was Made for Lovin' You", "duracion" : 268, "interprete" : "Kiss" }
+        ];
         return this.listaPistas;
     }
 }
