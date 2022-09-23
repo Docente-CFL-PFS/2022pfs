04a444d49d5e6872043500396c12e50f9feaaf7e
mauri52 <mauricio.islas@gmail.com>
2022-09-23 14:23:41 -0300

    paso 3.2 - servidor mockeado por api-rest - datos leidos desde archivo - cRud - correccion mostrarPistas

--- app/ejemploPistas.js
+++ app/ejemploPistas.js
@@ -61,6 +61,7 @@ function mostrarPistas() {
             <td>${r.titulo}</td>
             <td>${r.duracion}</td>
             <td>${r.interprete}</td>
+            <td></td>
             </tr>
         `; 
     }
